514823ea24f4ba61d08e4b1a918de37043161aab
Eryk Gasiorowski <erykgasiorowski@Eryks-Mac-mini.local>
2021-01-12 20:35:53 +0100

    modified gitignore to stop tracking Constants.swift, added ConstantsCopy.swift instead

--- youtube-onedaybuild/ConstantsCopy.swift
+++ youtube-onedaybuild/ConstantsCopy.swift
@@ -1,15 +1,17 @@
 //
-//  Constants.swift
+//  ConstantsCopy.swift
 //  youtube-onedaybuild
 //
-//  Created by Eryk Gasiorowski on 10/01/2021.
+//  Created by Eryk Gasiorowski on 12/01/2021.
 //
 
 import Foundation
 
+/*
 struct Constants {
     
     static var API_KEY = ""
-    static var PLAYLIST_ID = "PLMRqhzcHGw1YqPh-ggQHJPAUxdHov_uNJ"
+    static var PLAYLIST_ID = ""
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
+ /**/*/
