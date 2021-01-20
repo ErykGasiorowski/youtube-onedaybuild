7cba4fd575c548aca2cf592a93d2e8d4ac4a0ca4
Eryk Gasiorowski <erykgasiorowski@Eryks-Mac-mini.local>
2021-01-20 18:16:26 +0100

    added custom tableview cell, configured cell to show video thumbnail, title and date. Also added cache manager to save image data

--- youtube-onedaybuild/Models/Model.swift
+++ youtube-onedaybuild/Models/Model.swift
@@ -7,7 +7,14 @@
 
 import Foundation
 
+protocol ModelDelegate {
+    
+    func videosFetched(_ videos:[Video])
+}
+
 class Model {
+    
+    var delegate:ModelDelegate?
     
     func getVideos() {
         
@@ -36,10 +43,19 @@ class Model {
                 
                 let response = try decoder.decode(Response.self, from: data!)
                 
-                dump(response)
+                if response.items != nil {
+                    
+                    DispatchQueue.main.async {
+                        
+                        // Call the "videosFetched" method of the delegate
+                        self.delegate?.videosFetched(response.items!)
+                    }
+                }
+                
+                // dump(response)
             }
             catch {
-                print("Error Parsing the data into video objects")
+                
             }
             
         }
